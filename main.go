5ec87923f86927baef09ae5e66a6dfbdf496df87
Sindre Brurberg <sindre@brurberg.no>
2022-01-18 17:07:31 +0100

    changed how i find running to try and not restart all the time

--- main.go
+++ main.go
@@ -149,7 +149,8 @@ func main() {
 		}
 		ex := fmt.Sprintf("%s/%s", wd, artifactId)
 		if !foundNewerVersion {
-			cmd := exec.Command("ps", "h", "-C", ex)
+			//cmd := exec.Command("ps", "h", "-C", ex)
+			cmd := exec.Command("pgrep", "-u", "$(whoami)", artifactId) //Testing something
 			out, err := cmd.Output()
 			if err != nil {
 				log.Println(err)
